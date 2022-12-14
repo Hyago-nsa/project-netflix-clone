82954f0d8cab2c3dca1bd0097c5b552aa4d552c5
Hyago-nsa <hyago.eurico.nsa@gmail.com>
2022-12-14 16:55:12 -0300

    Adding Jumbotron
    Jumbotron starting files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import Jumbotron from "./components/jumbotron";
 
 const App = () => {
   return (
     <div>
       <h1>App</h1>
+      <Jumbotron/>
     </div>
   );
 };
