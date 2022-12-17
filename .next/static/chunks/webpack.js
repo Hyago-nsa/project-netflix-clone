abb61e568bbefb93f08d3b20f8f3781e342c279c
Hyago-nsa <hyago.eurico.nsa@gmail.com>
2022-12-17 17:25:24 -0300

    Add custom class

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "38f820f5500bf12a"; }
+/******/ 		__webpack_require__.h = function() { return "e7e0319708843443"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
