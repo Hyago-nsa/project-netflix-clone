020dd82c687cc4637bc5e478430547f934a4f4e4
Hyago-nsa <hyago.eurico.nsa@gmail.com>
2022-12-22 00:37:49 -0300

    Add Banner

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a1a632cb63b871e7"; }
+/******/ 		__webpack_require__.h = function() { return "2e5c46c5e0509d07"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.tsx
+++ pages/index.tsx
@@ -25,7 +25,7 @@ const Home = ({
   trendingNow,
 }: Props) => {
   return (
-    <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
+    <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[100vh]">
       <Head>
         <title>Netflix</title>
         <link rel="icon" href="/favicon.ico" />
@@ -33,7 +33,7 @@ const Home = ({
 
       <Header />
       <main>
-        <Banner />
+        <Banner netflixOriginals={netflixOriginals} />
         <section>
           {/* Row */}
           {/* Row */}
