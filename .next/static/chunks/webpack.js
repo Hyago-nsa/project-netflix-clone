cb982251e396092989b6fb946a304101496c7e2f
Hyago-nsa <hyago.eurico.nsa@gmail.com>
2022-12-21 23:35:38 -0300

    Add TypeScript Types

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "abbeaa9791eba596"; }
+/******/ 		__webpack_require__.h = function() { return "a1a632cb63b871e7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
